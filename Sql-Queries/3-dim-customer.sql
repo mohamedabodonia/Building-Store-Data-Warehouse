721b35302a6f6337eaab2c6b82c054fc4e9c62ed
mohamed ahmed <87114046+mohamedabodonia@users.noreply.github.com>
2025-01-07 16:14:08 +0200

    Update 3-dim-customer.sql

--- Sql-Queries/3-dim-customer.sql
+++ Sql-Queries/3-dim-customer.sql
@@ -2,21 +2,7 @@ USE EO_AdventureWorksDW2014
 
 go
 
--- Drop foregin Keys if exists
-IF EXISTS (SELECT *
-           FROM   sys.foreign_keys
-           WHERE  NAME = 'fk_fact_sales_dim_customer'
-                  AND parent_object_id = Object_id('fact_sales'))
-  ALTER TABLE fact_sales
-    DROP CONSTRAINT fk_fact_sales_dim_customer;
 
--- Drop and create the table
-IF EXISTS (SELECT *
-           FROM   sys.tables
-           WHERE  NAME = 'dim_customer')
-  DROP TABLE dim_customer
-
-go
 
 CREATE TABLE dim_customer
   (
@@ -40,10 +26,7 @@ CREATE TABLE dim_customer
   );
 
 -- Create Foreign Keys
-IF EXISTS (SELECT *
-           FROM   sys.tables
-           WHERE  NAME = 'fact_sales'
-                  AND type = 'u')
+
   ALTER TABLE fact_sales
     ADD CONSTRAINT fk_fact_sales_dim_customer FOREIGN KEY (customer_key)
     REFERENCES dim_customer(customer_key);
@@ -78,24 +61,11 @@ VALUES      (0,
 SET IDENTITY_INSERT dim_customer OFF
 
 -- Create Indexes
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'dim_customer_customer_id'
-                  AND object_id = Object_id('dim_customer'))
-  DROP INDEX dim_customer.dim_customer_customer_id
-
-go
-
+           
 CREATE INDEX dim_customer_customer_id
   ON dim_customer(customer_id);
 
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'dim_customer_city'
-                  AND object_id = Object_id('dim_customer'))
-  DROP INDEX dim_customer.dim_customer_city
 
-go
 
 CREATE INDEX dim_customer_city
   ON dim_customer(city); 
