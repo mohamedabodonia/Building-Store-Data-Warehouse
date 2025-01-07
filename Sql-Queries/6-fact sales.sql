e1e25d2fc5b33057d3a367f4253f0b8560ebb02b
mohamed ahmed <87114046+mohamedabodonia@users.noreply.github.com>
2025-01-07 20:32:42 +0200

    Update 6-fact sales.sql

--- Sql-Queries/6-fact sales.sql
+++ Sql-Queries/6-fact sales.sql
@@ -2,11 +2,7 @@ USE EO_AdventureWorksDW2014
 
 go
 
-IF EXISTS (SELECT *
-           FROM   sys.tables
-           WHERE  NAME = 'fact_sales')
-  DROP TABLE fact_sales;
-
+ 
 CREATE TABLE fact_sales
   (
      product_key    INT NOT NULL,
@@ -36,38 +32,18 @@ CREATE TABLE fact_sales
   );
 
 -- Create Indexes
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'fact_sales_dim_product'
-                  AND object_id = Object_id('fact_sales'))
-  DROP INDEX fact_sales.fact_sales_dim_product;
 
 CREATE INDEX fact_sales_dim_product
   ON fact_sales(product_key);
 
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'fact_sales_dim_customer'
-                  AND object_id = Object_id('fact_sales'))
-  DROP INDEX fact_sales.fact_sales_dim_customer;
 
 CREATE INDEX fact_sales_dim_customer
   ON fact_sales(customer_key);
 
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'fact_sales_dim_territory'
-                  AND object_id = Object_id('fact_sales'))
-  DROP INDEX fact_sales.fact_sales_dim_territory;
 
 CREATE INDEX fact_sales_dim_territory
   ON fact_sales(territory_key);
 
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'fact_sales_dim_date'
-                  AND object_id = Object_id('fact_sales'))
-  DROP INDEX fact_sales.fact_sales_dim_date;
 
 CREATE INDEX fact_sales_dim_date
   ON fact_sales(order_date_key); 
